0ba50d18900f42ac9d7fa388cd0c808ee7c6ae12
Keerthikanth Rohit. A <rohitkeerthikanth@gmail.com>
2025-12-03 04:23:31 +0530

    added a sentinel agent

--- packages/agents/defi-portfolio/src/agents/agent.ts
+++ packages/agents/defi-portfolio/src/agents/agent.ts
@@ -1,11 +1,15 @@
 import { AgentBuilder } from "@iqai/adk";
 import { model } from "../env";
+import { getStrategySentinelAgent } from "./sub-agents/strategy-sentinel-agent/agent";
+import { StrategyAaveV3ABI } from "./shared/abi";
 
 export const defiAgent = async () => {
+    const strategySentinalAgent = await getStrategySentinelAgent();
     return await AgentBuilder
         .create("DeFi_Portfolio_Agent")
         .withDescription("Portfolio Analyser and Market Advisor")
         .withInstruction("Analyses User's Potfolio and suggest actions")
         .withModel(model)
+        .withSubAgents([strategySentinalAgent])
         .build();
 }
