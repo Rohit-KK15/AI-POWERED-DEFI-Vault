761b2cfecc71aa46a8ece5db8dd545f5cc35c6ed
Keerthikanth Rohit. A <rohitkeerthikanth@gmail.com>
2025-12-03 06:44:26 +0530

    added Frontend and configured NextJS

---
+++ packages/frontend/next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        encoding: false,
+        "pino-pretty": false,
+      };
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
+
